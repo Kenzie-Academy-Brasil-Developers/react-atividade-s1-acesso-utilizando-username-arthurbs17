9c1bbaaafd1441ea20341e351b964893c383b466
Arthur Sarmento <arthurbs17@gmail.com>
2021-09-06 15:04:30 -0300

    Primeiro commit

---
+++ src/components/WelcomePage/index.jsx
@@ -0,0 +1,14 @@
+const WelcomePage = ({ user, setIsLoggedIn }) => {
+  const HandleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div>
+      <p>Bem vindo, {user}!</p>
+      <button onClick={HandleLogout}>Sair</button>
+    </div>
+  );
+};
+
+export default WelcomePage;
